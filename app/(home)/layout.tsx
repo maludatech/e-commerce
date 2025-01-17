1f946ec3906e777bf6781ae1e1a9af57de812dd2
maluda <nwitevictor7@gmail.com>
2025-01-17 17:20:13 +0100

    added (home) route

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/ui/shared/header";
+import Footer from "@/components/ui/shared/footer";
+
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1 flex flex-col">{children}</main>
+      <Footer />
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -29,11 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex flex-col min-h-screen">
-          <Header />
-          <main className="flex-1 flex flex-col">{children}</main>
-          <Footer />
-        </div>
+        {children}
       </body>
     </html>
   );

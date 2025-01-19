f771f58e7aec974fc614d85cca4f594f17138688
maluda <nwitevictor7@gmail.com>
2025-01-19 17:59:13 +0100

    added database seeding script

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,26 @@ import { ProductInputSchema } from "@/lib/validator";
 import { z } from "zod";
 
 export type IProductInput = z.infer<typeof ProductInputSchema>;
+
+export type Data = {
+  //   settings: ISettingInput[];
+  //   webPages: IWebPageInput[];
+  //   users: IUserInput[];
+  products: IProductInput[];
+  //   reviews: {
+  //     title: string;
+  //     rating: number;
+  //     comment: string;
+  //   }[];
+  headerMenus: {
+    name: string;
+    href: string;
+  }[];
+  carousels: {
+    image: string;
+    url: string;
+    title: string;
+    buttonCaption: string;
+    isPublished: boolean;
+  }[];
+};

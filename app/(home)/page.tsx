c521b5ed2742b9dd1ebab199f97cfc007e3faa97
maluda <nwitevictor7@gmail.com>
2025-01-20 15:11:50 +0100

    added home card component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,68 @@
+import { HomeCard } from "@/components/ui/shared/home/home-card";
 import { HomeCarousel } from "@/components/ui/shared/home/home-carousel";
+import {
+  getAllCategories,
+  getProductsForCard,
+} from "@/lib/actions/product.action";
 import data from "@/lib/data";
+import { toSlug } from "@/lib/utils";
 
 export default async function Page() {
-  return <HomeCarousel items={data.carousels} />;
+  const categories = (await getAllCategories()).slice(0, 4);
+  const newArrivals = await getProductsForCard({
+    tag: "new-arrival",
+  });
+  const featureds = await getProductsForCard({
+    tag: "featured",
+  });
+  const bestSellers = await getProductsForCard({
+    tag: "best-seller",
+  });
+  const cards = [
+    {
+      title: "Categories to explore",
+      link: {
+        text: "See More",
+        href: "/search",
+      },
+      items: categories.map((category) => ({
+        name: category,
+        image: `/assets/images/${toSlug(category)}.jpg`,
+        href: `/search?category=${category}`,
+      })),
+    },
+    {
+      title: "Explore New Arrivals",
+      items: newArrivals,
+      link: {
+        text: "View All",
+        href: "/search?tag=new-arrival",
+      },
+    },
+    {
+      title: "Discover Best Sellers",
+      items: bestSellers,
+      link: {
+        text: "View All",
+        href: "/search?tag=new-arrival",
+      },
+    },
+    {
+      title: "Featured Products",
+      items: featureds,
+      link: {
+        text: "Shop Now",
+        href: "/search?tag=new-arrival",
+      },
+    },
+  ];
+
+  return (
+    <div className="body-container">
+      <HomeCarousel items={data.carousels} />
+      <div className="md:p-4 md:space-y-4 bg-border">
+        <HomeCard cards={cards} />
+      </div>
+    </div>
+  );
 }
---
+++ lib/actions/product.action.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import { connectToDb } from "@/utils/database";
+import { IProduct } from "@/db/models/product.model";
+import Product from "@/db/models/product.model";
+
+export async function getAllCategories() {
+  await connectToDb();
+  const categories = await Product.find({ isPublished: true }).distinct(
+    "category"
+  );
+  return categories;
+}
+export async function getProductsForCard({
+  tag,
+  limit = 4,
+}: {
+  tag: string;
+  limit?: number;
+}) {
+  await connectToDb();
+  const products = await Product.find(
+    { tags: { $in: [tag] }, isPublished: true },
+    {
+      name: 1,
+      href: { $concat: ["/product/", "$slug"] },
+      image: { $arrayElemAt: ["$images", 0] },
+    }
+  )
+    .sort({ createdAt: "desc" })
+    .limit(limit);
+  return JSON.parse(JSON.stringify(products)) as {
+    name: string;
+    href: string;
+    image: string;
+  }[];
+}
+
+// GET PRODUCTS BY TAG
+export async function getProductsByTag({
+  tag,
+  limit = 10,
+}: {
+  tag: string;
+  limit?: number;
+}) {
+  await connectToDb();
+  const products = await Product.find({
+    tags: { $in: [tag] },
+    isPublished: true,
+  })
+    .sort({ createdAt: "desc" })
+    .limit(limit);
+  return JSON.parse(JSON.stringify(products)) as IProduct[];
+}

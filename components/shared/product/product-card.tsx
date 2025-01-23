2af488b9eb5f347da161085e7fec13e026242290
maluda <nwitevictor7@gmail.com>
2025-01-23 02:42:15 +0100

    updated product-card.tsx

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -10,9 +10,10 @@ import {
 import { IProduct } from "@/db/models/product.model";
 
 import Rating from "./rating";
-import { formatNumber } from "@/lib/utils";
+import { formatNumber, generateId, round2 } from "@/lib/utils";
 import ProductPrice from "./product-price";
 import ImageHover from "./image-hover";
+import AddToCart from "./add-to-card";
 
 const ProductCard = ({
   product,
@@ -75,26 +76,26 @@ const ProductCard = ({
       />
     </div>
   );
-  //   const AddButton = () => (
-  //     <div className="w-full text-center">
-  //       <AddToCart
-  //         minimal
-  //         item={{
-  //           clientId: generateId(),
-  //           product: product._id,
-  //           size: product.sizes[0],
-  //           color: product.colors[0],
-  //           countInStock: product.countInStock,
-  //           name: product.name,
-  //           slug: product.slug,
-  //           category: product.category,
-  //           price: round2(product.price),
-  //           quantity: 1,
-  //           image: product.images[0],
-  //         }}
-  //       />
-  //     </div>
-  //   );
+  const AddButton = () => (
+    <div className="w-full text-center">
+      <AddToCart
+        minimal
+        item={{
+          clientId: generateId(),
+          product: product._id,
+          size: product.sizes[0],
+          color: product.colors[0],
+          countInStock: product.countInStock,
+          name: product.name,
+          slug: product.slug,
+          category: product.category,
+          price: round2(product.price),
+          quantity: 1,
+          image: product.images[0],
+        }}
+      />
+    </div>
+  );
 
   return hideBorder ? (
     <div className="flex flex-col">
@@ -104,7 +105,7 @@ const ProductCard = ({
           <div className="p-3 flex-1 text-center">
             <ProductDetails />
           </div>
-          {/* {!hideAddToCart && <AddButton />} */}
+          {!hideAddToCart && <AddButton />}
         </>
       )}
     </div>
@@ -119,7 +120,7 @@ const ProductCard = ({
             <ProductDetails />
           </CardContent>
           <CardFooter className="p-3">
-            {/* {!hideAddToCart && <AddButton />} */}
+            {!hideAddToCart && <AddButton />}
           </CardFooter>
         </>
       )}

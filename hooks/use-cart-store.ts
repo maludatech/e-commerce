3821316eed23e98a34e013ccae4e4f50ede99303
maluda <nwitevictor7@gmail.com>
2025-01-23 15:28:05 +0100

    updated cart-store.tsZ

--- hooks/use-cart-store.ts
+++ hooks/use-cart-store.ts
@@ -18,6 +18,12 @@ const initialState: Cart = {
 interface CartState {
   cart: Cart;
   addItem: (item: OrderItem, quantity: number) => Promise<string>;
+  updateItem: (item: OrderItem, quantity: number) => Promise<void>;
+  removeItem: (item: OrderItem) => void;
+  clearCart: () => void;
+  setShippingAddress: (shippingAddress: ShippingAddress) => Promise<void>;
+  setPaymentMethod: (paymentMethod: string) => void;
+  setDeliveryDateIndex: (index: number) => Promise<void>;
 }
 
 const useCartStore = create(
@@ -60,6 +66,7 @@ const useCartStore = create(
             items: updatedCartItems,
             ...(await calculateDeliveryDateAndPrice({
               items: updatedCartItems,
+              // shippingAddress,
             })),
           },
         });
@@ -73,6 +80,95 @@ const useCartStore = create(
           throw new Error("Item not found in cart");
         }
         return foundItem.clientId;
+      },
+      updateItem: async (item: OrderItem, quantity: number) => {
+        const { items, shippingAddress } = get().cart;
+        const exist = items.find(
+          (x) =>
+            x.product === item.product &&
+            x.color === item.color &&
+            x.size === item.size
+        );
+        if (!exist) return;
+        const updatedCartItems = items.map((x) =>
+          x.product === item.product &&
+          x.color === item.color &&
+          x.size === item.size
+            ? { ...exist, quantity: quantity }
+            : x
+        );
+        set({
+          cart: {
+            ...get().cart,
+            items: updatedCartItems,
+            ...(await calculateDeliveryDateAndPrice({
+              items: updatedCartItems,
+              // shippingAddress,
+            })),
+          },
+        });
+      },
+      removeItem: async (item: OrderItem) => {
+        const { items, shippingAddress } = get().cart;
+        const updatedCartItems = items.filter(
+          (x) =>
+            x.product !== item.product ||
+            x.color !== item.color ||
+            x.size !== item.size
+        );
+        set({
+          cart: {
+            ...get().cart,
+            items: updatedCartItems,
+            ...(await calculateDeliveryDateAndPrice({
+              items: updatedCartItems,
+              // shippingAddress,
+            })),
+          },
+        });
+      },
+      setShippingAddress: async (shippingAddress: ShippingAddress) => {
+        const { items } = get().cart;
+        set({
+          cart: {
+            ...get().cart,
+            shippingAddress,
+            ...(await calculateDeliveryDateAndPrice({
+              items,
+              // shippingAddress,
+            })),
+          },
+        });
+      },
+      setPaymentMethod: (paymentMethod: string) => {
+        set({
+          cart: {
+            ...get().cart,
+            paymentMethod,
+          },
+        });
+      },
+      setDeliveryDateIndex: async (index: number) => {
+        const { items, shippingAddress } = get().cart;
+
+        set({
+          cart: {
+            ...get().cart,
+            ...(await calculateDeliveryDateAndPrice({
+              items,
+              // shippingAddress,
+              deliveryDateIndex: index,
+            })),
+          },
+        });
+      },
+      clearCart: () => {
+        set({
+          cart: {
+            ...get().cart,
+            items: [],
+          },
+        });
       },
       init: () => set({ cart: initialState }),
     }),

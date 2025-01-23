b4f25b06caf103f8b38e493efd59d40898af80f5
maluda <nwitevictor7@gmail.com>
2025-01-23 01:41:12 +0100

    added cart-store.tsx

---
+++ hooks/use-cart-store.ts
@@ -0,0 +1,85 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+import { Cart, OrderItem, ShippingAddress } from "@/types";
+import { calculateDeliveryDateAndPrice } from "@/lib/actions/order.action";
+
+const initialState: Cart = {
+  items: [],
+  itemsPrice: 0,
+  taxPrice: undefined,
+  shippingPrice: undefined,
+  totalPrice: 0,
+  paymentMethod: undefined,
+  shippingAddress: undefined,
+  deliveryDateIndex: undefined,
+};
+
+interface CartState {
+  cart: Cart;
+  addItem: (item: OrderItem, quantity: number) => Promise<string>;
+}
+
+const useCartStore = create(
+  persist<CartState>(
+    (set, get) => ({
+      cart: initialState,
+
+      addItem: async (item: OrderItem, quantity: number) => {
+        const { items, shippingAddress } = get().cart;
+        const existItem = items.find(
+          (x) =>
+            x.product === item.product &&
+            x.color === item.color &&
+            x.size === item.size
+        );
+
+        if (existItem) {
+          if (existItem.countInStock < quantity + existItem.quantity) {
+            throw new Error("Not enough items in stock");
+          }
+        } else {
+          if (item.countInStock < item.quantity) {
+            throw new Error("Not enough items in stock");
+          }
+        }
+
+        const updatedCartItems = existItem
+          ? items.map((x) =>
+              x.product === item.product &&
+              x.color === item.color &&
+              x.size === item.size
+                ? { ...existItem, quantity: existItem.quantity + quantity }
+                : x
+            )
+          : [...items, { ...item, quantity }];
+
+        set({
+          cart: {
+            ...get().cart,
+            items: updatedCartItems,
+            ...(await calculateDeliveryDateAndPrice({
+              items: updatedCartItems,
+            })),
+          },
+        });
+        const foundItem = updatedCartItems.find(
+          (x) =>
+            x.product === item.product &&
+            x.color === item.color &&
+            x.size === item.size
+        );
+        if (!foundItem) {
+          throw new Error("Item not found in cart");
+        }
+        return foundItem.clientId;
+      },
+      init: () => set({ cart: initialState }),
+    }),
+
+    {
+      name: "cart-store",
+    }
+  )
+);
+export default useCartStore;

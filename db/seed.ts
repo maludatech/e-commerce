1b077091e105000a5bddb7e32d47f57940b64a9f
maluda <nwitevictor7@gmail.com>
2025-01-25 20:09:28 +0100

    updated seed.ts

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,7 @@
 import data from "@/lib/data";
 import { connectToDb } from "@/utils/database";
 import Product from "./models/product.model";
+import User from "./models/user.model";
 import { cwd } from "process";
 import { loadEnvConfig } from "@next/env";
 
@@ -8,13 +9,18 @@ loadEnvConfig(cwd());
 
 const main = async () => {
   try {
-    const { products } = data;
+    const { products, users } = data;
     await connectToDb();
+
+    await User.deleteMany();
+    const createdUsers = await User.insertMany(users);
 
     await Product.deleteMany();
     const createdProducts = await Product.insertMany(products);
+
     console.log({
       createdProducts,
+      createdUsers,
       message: "Seeded database successfully",
     });
     process.exit(0);

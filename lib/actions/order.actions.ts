fab156d529c73f89f3e8537bab294315798557e5
maluda <nwitevictor7@gmail.com>
2025-01-30 17:42:56 +0100

    added pagination to my orders

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -2,7 +2,7 @@
 
 import { Cart, OrderItem, ShippingAddress } from "@/types";
 import { formatError, round2 } from "../utils";
-import { AVAILABLE_DELIVERY_DATES } from "../constants";
+import { AVAILABLE_DELIVERY_DATES, PAGE_SIZE } from "../constants";
 import { connectToDb } from "@/utils/database";
 import { auth } from "@/auth";
 import { OrderInputSchema } from "../validator";
@@ -104,6 +104,38 @@ export const createOrderFromCart = async (
   });
   return await Order.create(order);
 };
+
+export async function getMyOrders({
+  limit,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  // const {
+  //   common: { pageSize },
+  // } = await getSetting();
+
+  limit = limit || PAGE_SIZE;
+  await connectToDb();
+  const session = await auth();
+  if (!session) {
+    throw new Error("User is not authenticated");
+  }
+  const skipAmount = (Number(page) - 1) * limit;
+  const orders = await Order.find({
+    user: session?.user?.id,
+  })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(limit);
+  const ordersCount = await Order.countDocuments({ user: session?.user?.id });
+
+  return {
+    data: JSON.parse(JSON.stringify(orders)),
+    totalPages: Math.ceil(ordersCount / limit),
+  };
+}
 
 export async function getOrderById(orderId: string): Promise<IOrder> {
   await connectToDb();

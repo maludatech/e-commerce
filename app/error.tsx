cba55cb5e3b577f59860adc6c3f507b0fa4ee157
maluda <nwitevictor7@gmail.com>
2025-01-31 17:02:40 +0100

    updated not-found.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 import { Button } from "@/components/ui/button";
-import { useTranslations } from "next-intl";
+// import { useTranslations } from "next-intl";
 
 export default function ErrorPage({
   error,
@@ -11,21 +11,25 @@ export default function ErrorPage({
   error: Error;
   reset: () => void;
 }) {
-  const t = useTranslations();
+  //   const t = useTranslations();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen ">
       <div className="p-6 rounded-lg shadow-md w-1/3 text-center">
-        <h1 className="text-3xl font-bold mb-4">{t("Error.Error")}</h1>
+        <h1 className="text-3xl font-bold mb-4">
+          {/*{t("Error.Error")}}*/} Error
+        </h1>
         <p className="text-destructive">{error.message}</p>
         <Button variant="outline" className="mt-4" onClick={() => reset()}>
-          {t("Error.Try again")}
+          {/* {t("Error.Try again")} */}
+          Try again
         </Button>
         <Button
           variant="outline"
           className="mt-4 ml-2"
           onClick={() => (window.location.href = "/")}
         >
-          {t("Error.Back To Home")}
+          {/* {t("Error.Back To Home")} */}
+          Back To Home
         </Button>
       </div>
     </div>

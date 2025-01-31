ac0029e2f3b4d4cc6ce3c64c4f3d65e617f356cf
maluda <nwitevictor7@gmail.com>
2025-01-31 01:00:09 +0100

    updated product.actions.ts

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,6 +3,7 @@
 import { connectToDb } from "@/utils/database";
 import { IProduct } from "@/db/models/product.model";
 import Product from "@/db/models/product.model";
+import { PAGE_SIZE } from "../constants";
 
 // GET ALL CATEGORIES
 export async function getAllCategories() {
@@ -92,5 +93,101 @@ export async function getRelatedProductsByCategory({
   return {
     data: JSON.parse(JSON.stringify(products)) as IProduct[],
     totalPages: Math.ceil(productsCount / limit),
+  };
+}
+
+export async function getAllProducts({
+  query,
+  limit,
+  page,
+  category,
+  tag,
+  price,
+  rating,
+  sort,
+}: {
+  query: string;
+  category: string;
+  tag: string;
+  limit?: number;
+  page: number;
+  price?: string;
+  rating?: string;
+  sort?: string;
+}) {
+  // const {
+  //   common: { pageSize },
+  // } = await getSetting();
+
+  limit = limit || PAGE_SIZE;
+  await connectToDb();
+
+  const queryFilter =
+    query && query !== "all"
+      ? {
+          name: {
+            $regex: query,
+            $options: "i",
+          },
+        }
+      : {};
+  const categoryFilter = category && category !== "all" ? { category } : {};
+  const tagFilter = tag && tag !== "all" ? { tags: tag } : {};
+
+  const ratingFilter =
+    rating && rating !== "all"
+      ? {
+          avgRating: {
+            $gte: Number(rating),
+          },
+        }
+      : {};
+  // 10-50
+  const priceFilter =
+    price && price !== "all"
+      ? {
+          price: {
+            $gte: Number(price.split("-")[0]),
+            $lte: Number(price.split("-")[1]), //Example: 100 - 500 dollars
+          },
+        }
+      : {};
+  const order: Record<string, 1 | -1> =
+    sort === "best-selling"
+      ? { numSales: -1 }
+      : sort === "price-low-to-high"
+        ? { price: 1 }
+        : sort === "price-high-to-low"
+          ? { price: -1 }
+          : sort === "avg-customer-review"
+            ? { avgRating: -1 }
+            : { _id: -1 };
+  const isPublished = { isPublished: true };
+  const products = await Product.find({
+    ...isPublished,
+    ...queryFilter,
+    ...tagFilter,
+    ...categoryFilter,
+    ...priceFilter,
+    ...ratingFilter,
+  })
+    .sort(order)
+    .skip(limit * (Number(page) - 1))
+    .limit(limit)
+    .lean();
+
+  const countProducts = await Product.countDocuments({
+    ...queryFilter,
+    ...tagFilter,
+    ...categoryFilter,
+    ...priceFilter,
+    ...ratingFilter,
+  });
+  return {
+    products: JSON.parse(JSON.stringify(products)) as IProduct[],
+    totalPages: Math.ceil(countProducts / limit),
+    totalProducts: countProducts,
+    from: limit * (Number(page) - 1) + 1,
+    to: limit * (Number(page) - 1) + products.length,
   };
 }

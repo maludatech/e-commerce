4c76dc431e9166cf3525a6d9d89f6f07e026d0fd
maluda <nwitevictor7@gmail.com>
2025-02-24 17:27:11 +0100

    added product list

--- components/shared/client-providers.tsx
+++ components/shared/client-providers.tsx
@@ -4,31 +4,31 @@ import useCartSidebar from "@/hooks/use-cart-sidebar";
 import CartSidebar from "./cart-sidebar";
 import { ThemeProvider } from "./theme-provider";
 import { Toaster } from "../ui/toaster";
-// import AppInitializer from './app-initializer'
-// import { ClientSetting } from "@/types";
+import AppInitializer from "./app-initializer";
+import { ClientSetting } from "@/types";
 
 export default function ClientProviders({
-  // setting,
+  setting,
   children,
 }: {
-  // setting: ClientSetting;
+  setting: ClientSetting;
   children: React.ReactNode;
 }) {
   const visible = useCartSidebar();
 
   return (
-    // <AppInitializer setting={setting}>
-    <ThemeProvider attribute="class" defaultTheme="light">
-      {visible ? (
-        <div className="flex min-h-screen">
-          <div className="flex-1 overflow-hidden">{children}</div>
-          <CartSidebar />
-        </div>
-      ) : (
-        <div>{children}</div>
-      )}
-      <Toaster />
-    </ThemeProvider>
-    // </AppInitializer>
+    <AppInitializer setting={setting}>
+      <ThemeProvider attribute="class" defaultTheme="light">
+        {visible ? (
+          <div className="flex min-h-screen">
+            <div className="flex-1 overflow-hidden">{children}</div>
+            <CartSidebar />
+          </div>
+        ) : (
+          <div>{children}</div>
+        )}
+        <Toaster />
+      </ThemeProvider>
+    </AppInitializer>
   );
 }

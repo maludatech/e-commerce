9c2a13d80e98d32a97bcfdb8ad46257fe30224e3
maluda <nwitevictor7@gmail.com>
2025-06-25 21:36:22 +0100

    Implemented Open graph details

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -30,6 +30,28 @@ export async function generateMetadata() {
     },
     description: description,
     metadataBase: new URL(url),
+    openGraph: {
+      title: name,
+      description: description,
+      url: url,
+      siteName: name,
+      images: [
+        {
+          url: "/assets/images/og-image.png",
+          width: 1200,
+          height: 630,
+          alt: `${name} Preview`,
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: name,
+      description: description,
+      images: ["/assets/images/og-image.png"],
+    },
   };
 }
 
